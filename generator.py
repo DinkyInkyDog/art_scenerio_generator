bd6f4eca2fb3e590810aab62d5d85c4147429793
DinkyInkyDog <142037259+DinkyInkyDog@users.noreply.github.com>
2023-10-27 14:51:50 -0400

    Correct my spelling of scenario

--- generator.py
+++ generator.py
@@ -1,15 +1,15 @@
 #i want to eventually be able to make it so someone could add thier own characters into the generator.
 
 default_characters = ["Dani", "Wren", "Josh", "Logan", "Blaine", "Tuesday", "Lillian", "TJ", "Indigo", "Sage", "Jay", "Dee", "Sarah", "Chiqua", "Nikki", "Weregamer", "Haven"]
-default_scenerios = [" goes on a date with ", " cooks for ", " has to save the life of ", " at the funeral of "]
+default_scenarios = [" goes on a date with ", " cooks for ", " has to save the life of ", " at the funeral of "]
 
 num_characters = len(default_characters)
-num_scenerios = len(default_scenerios)
+num_scenarios = len(default_scenarios)
 
 import random
 character_one = random.randint(0, num_characters)
 character_two = random.randint(0, num_characters)
-scenerio = random.randint(0, num_scenerios)
+scenario = random.randint(0, num_scenarios)
 
 if character_one == character_two:
     character_one = random.randint(0, num_characters)
@@ -17,4 +17,4 @@ if character_one == character_two:
 
 #final product for default gen
 print("imagine that...")
-print(default_characters[character_one], default_scenerios[scenerio], default_characters[character_two])
+print(default_characters[character_one], default_scenarios[scenario], default_characters[character_two])
