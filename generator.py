fbb374ea28d5d3785a2f862b7837554535e2e2fb
DinkyInkyDog <142037259+DinkyInkyDog@users.noreply.github.com>
2023-10-26 21:43:17 -0400

    add scenerios and the number for the lists

--- generator.py
+++ generator.py
@@ -1,8 +1,10 @@
 #i want to eventually be able to make it so someone could add thier own characters into the generator.
 
 default_characters = ["Dani", "Wren", "Josh", "Logan", "Blaine", "Tuesday", "Lillian", "TJ", "Indigo", "Sage", "Jay", "Dee", "Sarah", "Chiqua", "Nikki", "Weregamer", "Haven"]
-default_scenerio = ["", ]
+default_scenerios = [" goes on a date with ", " cooks for ", " has to save the life of ", " at the funeral of "]
 
+num_characters = len(default_characters)
+num_scenerios = len(default_scenerios)
 
 import random
 
