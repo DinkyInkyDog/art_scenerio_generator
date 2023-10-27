e8ba6371c637b7345636ac5eccc2bbb82a88cc32
DinkyInkyDog <142037259+DinkyInkyDog@users.noreply.github.com>
2023-10-26 21:38:05 -0400

    set up program basics

---
+++ generator.py
@@ -0,0 +1,12 @@
+#i want to eventually be able to make it so someone could add thier own characters into the generator.
+
+default_characters = ["Dani", "Wren", "Josh", "Logan", "Blaine", "Tuesday", "Lillian", "TJ", "Indigo", "Sage", "Jay", "Dee", "Sarah", "Chiqua", "Nikki", "Weregamer", "Haven"]
+default_scenerio = ["", ]
+
+
+import random
+
+
+#final product
+print("imagine that...")
+print(character_one, scenerio, character_two)
